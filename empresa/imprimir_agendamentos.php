fa79439d14d87349921abb760e7e63656c65818f
OC 2016 - Malta <occhapecosenai@gmail.com>
2016-09-29 14:37:56 -0300

    Imprimindo agendamentos

--- empresa/imprimir_agendamentos.php
+++ empresa/imprimir_agendamentos.php
@@ -1,124 +1,95 @@
 <?php
-	//define('FPDF_FONTPATH', 'font/');
-	require_once('fpdf181/fpdf.php');
-	require_once("../conectar_service.php");
-        if(!isset($_SESSION))
-                session_start();
 
-	$pdf = new FPDF ('P','cm','A4');
-	//$pdf->Open();
-	$pdf->AddPage();
-	$pdf->SetFont('Arial','','12');
-
-	if (isset($_POST["aceito"]))
+        if(isset($_POST))
         {
-                $json_dados = $service->call('agendamento.select_aceitos_by_empresa',array($_SESSION["id"]));
-                $aceito = json_decode($json_dados);
-                for($i=0; $i<count($aceito);$i++)
+                require_once('fpdf181/fpdf.php');
+                require_once("../conectar_service.php");
+                if(!isset($_SESSION))
+                        session_start();
+                $pdf = new FPDF ('L','cm','A4');
+                $pdf->AddPage();
+                $pdf->SetFont('Arial','b','12');
+                $pdf->Cell(4,1,utf8_decode('Solicitante'),1,0);
+                $pdf->Cell(3,1,utf8_decode('Telefone'),1,0);
+                $pdf->Cell(2.5,1,utf8_decode('Data'),1,0);
+                $pdf->Cell(2,1,utf8_decode('Horário'),1,0);
+                $pdf->Cell(16,1,utf8_decode('Endereço'),1,1);
+                $pdf->SetFont('Arial','','11');
+        	if (isset($_POST["aceito"]))
                 {
-                	$pdf->Cell(5,2,$aceito->data_agendamento,0,0);
-                	$pdf->Cell(5,2,$aceito->horario,0,0);
-                	$json_dados = $service->call('endereco.select_by_id',array($aceito[$i]->usuario_id));
-                	$endereco = json_decode($json_dados);
-                	$end = $endereco[0]->cidade;
-                	$end += ', ';
-                	$end += $endereco[0]->bairro;
-                	$end += ', ';
-                	$end += $endereco[0]->rua;
-                	$end +=', ';
-                	$end += $endereco[0]->num;
-                	$end +=', ';
-                	$end += $endereco[0]->complemento;
-                	$end += ', ';
-                	$end += $endereco[0]->cep;
-                	$pdf->Cell(5,2,$end,1,1);
-        		$pdf->OutPut();
-                }
-                header('location: agendamentos_aceitos.php');
-	}
-	
-	if (isset($_POST["realizado"]))
-        {
-                $json_dados = $service->call('agendamento.select_realizados_by_empresa',array($_SESSION["id"]));
-                $realizado = json_decode($json_dados);
-                for($i=0; $i<count($realizado);$i++)
+                        $json_dados = $service->call('agendamento.select_aceitos_by_empresa',array($_SESSION["id"]));
+                        $aceito = json_decode($json_dados);
+                        for($i=0; $i<count($aceito);$i++)
+                        {
+                                $json_dados = $service->call('usuario.select',array("id = ".$aceito[$i]->usuario_id));
+                                $usuario = json_decode($json_dados);
+                                $pdf->Cell(4,1,$usuario[0]->nome,1,0);
+                                $pdf->Cell(3,1,$usuario[0]->telefone,1,0);
+                                $pdf->Cell(2.5,1,$aceito[$i]->data_agendamento,1,0);
+                                $pdf->Cell(2,1,$aceito[$i]->horario,1,0);
+                                $json_dados = $service->call('endereco.select_by_id',array($aceito[$i]->endereco_id));
+                                $endereco = json_decode($json_dados);
+                                $end = utf8_decode($endereco[0]->rua." ".$endereco[0]->num.", ".$endereco[0]->cep.", ".$endereco[0]->bairro.", ".$endereco[0]->cidade." - ".$endereco[0]->uf.", ".$endereco[0]->pais);
+                                $pdf->Cell(16,1,$end,1,1);
+                        }
+        	}
+        	elseif (isset($_POST["realizado"]))
                 {
-                	$pdf->Cell(5,2,$realizado->data_agendamento,0,0);
-                	$pdf->Cell(5,2,$realizado->horario,0,0);
-                	$json_dados = $service->call('endereco.select_by_id',array($realizado->usuario_id));
-                	$endereco = json_decode($json_dados);
-                	$end = $endereco[0]->cidade;
-                	$end += ', ';
-                	$end += $endereco[0]->bairro;
-                	$end += ', ';
-                	$end += $endereco[0]->rua;
-                	$end +=', ';
-                	$end += $endereco[0]->num;
-                	$end +=', ';
-                	$end += $endereco[0]->complemento;
-                	$end += ', ';
-                	$end += $endereco[0]->cep;
-                	$pdf->Cell(5,2,$end,1,1);
-        		$pdf->OutPut();
-                }
-                header('location: agendamentos_realizados.php');
-	}
-	
-	if (isset($_POST["atrasado"]))
-        {
-                $json_dados = $service->call('agendamento.select_atrasados_by_empresa',array($_SESSION["id"]));
-                $atrasado = json_decode($json_dados);
-                for($i=0; $i<count($atrasado);$i++)
+                        $json_dados = $service->call('agendamento.select_realizados_by_empresa',array($_SESSION["id"]));
+                        $realizado = json_decode($json_dados);
+                        for($i=0; $i<count($realizado);$i++)
+                        {
+                                $json_dados = $service->call('usuario.select',array("id = ".$realizado[$i]->usuario_id));
+                                $usuario = json_decode($json_dados);
+                                $pdf->Cell(4,1,$usuario[0]->nome,1,0);
+                                $pdf->Cell(3,1,$usuario[0]->telefone,1,0);
+                        	$pdf->Cell(2.5,1,$realizado[$i]->data_agendamento,1,0);
+                        	$pdf->Cell(2,1,$realizado[$i]->horario,1,0);
+                        	$json_dados = $service->call('endereco.select_by_id',array($realizado[$i]->endereco_id));
+                        	$endereco = json_decode($json_dados);
+                                $end = utf8_decode($endereco[0]->rua." ".$endereco[0]->num.", ".$endereco[0]->cep.", ".$endereco[0]->bairro.", ".$endereco[0]->cidade." - ".$endereco[0]->uf.", ".$endereco[0]->pais);
+                        	$pdf->Cell(16,1,$end,1,1);
+                        }
+        	}
+        	elseif (isset($_POST["atrasado"]))
                 {
-                	$pdf->Cell(5,2,$atrasado->data_agendamento,0,0);
-                	$pdf->Cell(5,2,$atrasado->horario,0,0);
-                	$json_dados = $service->call('endereco.select_by_id',array($atrasado->usuario_id));
-                	$endereco = json_decode($json_dados);
-                	$end = $endereco[0]->cidade;
-                	$end += ', ';
-                	$end += $endereco[0]->bairro;
-                	$end += ', ';
-                	$end += $endereco[0]->rua;
-                	$end +=', ';
-                	$end += $endereco[0]->num;
-                	$end +=', ';
-                	$end += $endereco[0]->complemento;
-                	$end += ', ';
-                	$end += $endereco[0]->cep;
-                	$pdf->Cell(5,2,$end,1,1);
-        		$pdf->OutPut();
-                }
-                header('location: agendamentos_atradados.php');
-	}
-
-        if (isset($_POST["em_espera"]))
-        {
-                $json_dados = $service->call('agendamento.select_atrasados_by_empresa',array($_SESSION["id"]));
-                $atrasado = json_decode($json_dados);
-                for($i=0; $i<count($atrasado);$i++)
+                        $json_dados = $service->call('agendamento.select_atrasados_by_empresa',array($_SESSION["id"]));
+                        $atrasado = json_decode($json_dados);
+                        for($i=0; $i<count($atrasado);$i++)
+                        {
+                                $json_dados = $service->call('usuario.select',array("id = ".$atrasado[$i]->usuario_id));
+                                $usuario = json_decode($json_dados);
+                                $pdf->Cell(4,1,$usuario[0]->nome,1,0);
+                                $pdf->Cell(3,1,$usuario[0]->telefone,1,0);
+                                $pdf->Cell(2.5,1,$atrasado[$i]->data_agendamento,1,0);
+                                $pdf->Cell(2,1,$atrasado[$i]->horario,1,0);
+                                $json_dados = $service->call('endereco.select_by_id',array($atrasado[$i]->endereco_id));
+                                $endereco = json_decode($json_dados);
+                                $end = utf8_decode($endereco[0]->rua." ".$endereco[0]->num.", ".$endereco[0]->cep.", ".$endereco[0]->bairro.", ".$endereco[0]->cidade." - ".$endereco[0]->uf.", ".$endereco[0]->pais);
+                                $pdf->Cell(16,1,$end,1,1);
+                        }
+        	}
+                elseif (isset($_POST["em_espera"]))
                 {
-                        $pdf->Cell(5,2,$atrasado->data_agendamento,0,0);
-                        $pdf->Cell(5,2,$atrasado->horario,0,0);
-                        $json_dados = $service->call('endereco.select_by_id',array($atrasado->usuario_id));
-                        $endereco = json_decode($json_dados);
-                        $end = $endereco[0]->cidade;
-                        $end += ', ';
-                        $end += $endereco[0]->bairro;
-                        $end += ', ';
-                        $end += $endereco[0]->rua;
-                        $end +=', ';
-                        $end += $endereco[0]->num;
-                        $end +=', ';
-                        $end += $endereco[0]->complemento;
-                        $end += ', ';
-                        $end += $endereco[0]->cep;
-                        $pdf->Cell(5,2,$end,1,1);
-                        $pdf->OutPut();
+                        $json_dados = $service->call('agendamento.select_atrasados_by_empresa',array($_SESSION["id"]));
+                        $em_espera = json_decode($json_dados);
+                        for($i=0; $i<count($em_espera);$i++)
+                        {
+                                $json_dados = $service->call('usuario.select',array("id = ".$em_espera[$i]->usuario_id));
+                                $usuario = json_decode($json_dados);
+                                $pdf->Cell(4,1,$usuario[0]->nome,1,0);
+                                $pdf->Cell(3,1,$usuario[0]->telefone,1,0);
+                                $pdf->Cell(2.5,1,$em_espera[$i]->data_agendamento,1,0);
+                                $pdf->Cell(2,1,$em_espera[$i]->horario,1,0);
+                                $json_dados = $service->call('endereco.select_by_id',array($em_espera[$i]->endereco_id));
+                                $endereco = json_decode($json_dados);
+                                $end = utf8_decode($endereco[0]->rua." ".$endereco[0]->num.", ".$endereco[0]->cep.", ".$endereco[0]->bairro.", ".$endereco[0]->cidade." - ".$endereco[0]->uf.", ".$endereco[0]->pais);
+                                $pdf->Cell(16,1,$end,1,1);
+                        }
                 }
-                header('location: agendamentos_em_espera.php');
+                $pdf->OutPut();
         }
-
-
-	
+        else
+                header("location: index.php");
 
 ?>
