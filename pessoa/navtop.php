9480a0b27d7332fb85ff9a938d752a8de7f2dd13
OC - Gabriel <occhapecosenai@gmail.com>
2016-10-17 15:37:37 -0200

    mercy

--- pessoa/navtop.php
+++ pessoa/navtop.php
@@ -1,45 +1,10 @@
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Infinity - Mobile App HTML5 Template">
-	<meta name="keywords" content="keywords">
-	<meta name="author" content="Audain Designs">
+<!-- Bootstrap -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
 
-	<!-- Bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- General Style -->
+<link href="style1.css" rel="stylesheet">
 
-	<!-- General Style -->
-	<link href="style1.css" rel="stylesheet">
-	
-	<!--Custom CSS-->
-	<link href="custom.css" rel="stylesheet">
-
-	<!-- Animations -->
-	<link href="css/animate.css" rel="stylesheet">
-		
-	<!-- Vendor Styles -->
-	<link rel="stylesheet" type="text/css" href="vendor/push-menu/css/jasny-bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="vendor/push-menu/css/push-menu.css" />
-	<link rel="stylesheet" type="text/css" href="vendor/animated-text/css/style.css" />
-	
-	<!-- Device Styles -->
-	<link rel="stylesheet" type="text/css" href="css/devices/style.css">
-	<script src="https://use.fontawesome.com/9c8fd2c64e.js"></script>
-
-	<!-- Cover Styles (Skin) -->
-	<link rel="stylesheet" type="text/css" href="css/landing/landing.css">
-	
-	<!--Fonts-->
-	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,200,100,500,600,700,800,900' rel='stylesheet' type='text/css'>
-	<link href="css/fonts/font-awesome.css" rel="stylesheet">
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
+<script src="https://use.fontawesome.com/9c8fd2c64e.js"></script>
 <style>
 .mobile{
 	background-color:#606060;
@@ -49,7 +14,7 @@
 <header class="mobile">
   <div class="row">
     <div class="col full">
-      <div style="margin-left:30px;"><a href="index.php" class="logo" style="color:white;"><b>DescartesLab</b></a></div>
+      <div style="margin-left:10px;"><a href="index.php" class="logo" style="color:white;"><b>DescartesLab</b></a></div>
       <nav id="nav-wrap" style="left: 25%;">
         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">
         	<i class="fa fa-bars" style="color:white; margin-top:2px;margin-left:8px;font-size:36px;"></i>
@@ -61,7 +26,7 @@
             <li><a href="index.php">Mapa</a></li>
             <li><a href="enderecos.php">Endereços</a></li>
             <li><a href="pedidos.php">Agendamentos</a></li>
-            <li style="margin-left:250px;margin-right:20px;">
+            <li style="" class="logina">
                 <a href="notificacoes.php">
                     <?php
                         $json_dados = $service->call('notificacao.select_nao_visualizados_by_usuario',array($_SESSION["id"]));
@@ -69,7 +34,7 @@
                         if (count($notificacao) > 0) 
                             echo '<span class="badge bg-theme pull-right">'.count($notificacao).'</span>';
                     ?>
-                    <i class="fa fa-envelope-o" style="margin-bottom:5px;"></i>
+                    <i class="fa fa-envelope-o hidden-xm" style="margin-bottom:5px;"></i>
                 </a>
             </li>
 					
