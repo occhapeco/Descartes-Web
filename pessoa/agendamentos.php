ea29c751c7a5c1785f9839b5414a2e1b0a099282
OC - Gabriel <occhapecosenai@gmail.com>
2016-10-18 14:00:21 -0200

    nav-finalizada2

--- pessoa/agendamentos.php
+++ pessoa/agendamentos.php
@@ -56,7 +56,6 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="dist/css/bootstrap-select.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
