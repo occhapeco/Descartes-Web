e97042abeef72185ca65bdcb979b99f0d3d08f9b
occhapeco <occhapecosenai@gmail.com>
2016-10-20 10:24:05 -0200

    Alteração de título das páginas

--- pt/empresa/cadastro_pontos.php
+++ pt/empresa/cadastro_pontos.php
@@ -125,7 +125,7 @@
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-    <title>Descartes</title>
+    <title>DescartesLab</title>
 
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
