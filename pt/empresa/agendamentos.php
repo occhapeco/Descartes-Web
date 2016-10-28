20fdfac4275408ddc4a53b8668b5a44b444c9b54
OC - Gabriel <occhapecosenai@gmail.com>
2016-10-28 17:16:59 -0200

    agenda - empresa - pt

--- pt/empresa/agendamentos.php
+++ pt/empresa/agendamentos.php
@@ -1,4 +1,24 @@
-<?php require_once("permissao.php"); ?>
+<?php require_once("permissao.php"); 
+  require_once("../conectar_service.php"); 
+$active='home';
+
+if (isset($_POST["aceitar"]))
+  {
+    $bool = $service->call('agendamento.aceitar',array($_POST["aceitar"]));
+    $active='menu1';
+  }
+  
+if (isset($_POST["recusar"]))
+  {
+    $bool = $service->call('agendamento.recusar',array($_POST["recusar"]));
+  }
+  
+if (isset($_POST["cancelar"]))
+  {
+    $bool = $service->call('agendamento.cancelar',array($_POST["cancelar"]));
+    $active="menu4";
+  }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,8 +42,6 @@
     <link href="assets/css/style-responsive.css" rel="stylesheet">
     <link href="assets/css/table-responsive.css" rel="stylesheet">
 
-    <script src="assets/js/chart-master/Chart.js"></script>
-
   </head>
 
   <body>
@@ -39,20 +57,20 @@
       *********************************************************************************************************************************************************** -->
       <!--main content start-->
         <section class="wrapper">
-          <h3><i class="fa fa-angle-right"></i> Agendamentos</h3>
+          <h3><i class="fa fa-angle-right" style="margin-top:50px;"></i> Agendamentos</h3>
           <div class="row mt">
             <div class="col-lg-12">
               <div class="content-panel">
                 <ul class="nav nav-tabs" style="margin-left: 20px;">
-                  <li class="active"><a data-toggle="pill" href="#home" style="color: #797979;"><img src="images/agenda5.png" height="15px" width="15px"> Novos</a></li>
-                  <li><a data-toggle="pill" href="#menu1" style="color: #797979;"><img src="images/agenda1.png" height="15px" width="15px"> Pendentes</a></li>
-                  <li><a data-toggle="pill" href="#menu2" style="color: #797979;"><img src="images/agenda3.png" height="15px" width="15px"> Atrasados</a></li>
-                  <li><a data-toggle="pill" href="#menu3" style="color: #797979;"><img src="images/agenda2.png" height="15px" width="15px"> Realizados</a></li>
-                  <li><a data-toggle="pill" href="#menu4" style="color: #797979;"><img src="images/agenda4.png" height="15px" width="15px"> Cancelados</a></li>
+                  <li class="<?php if($active == "home") echo ' active';?>"><a data-toggle="pill" href="#home" style="color: #797979;"><img src="images/agenda5.png" height="15px" width="15px"> Novo</a></li>
+                  <li <?php if($active == "menu1") echo ' class="active"'; ?>><a data-toggle="pill" href="#menu1" style="color: #797979;"><img src="images/agenda1.png" height="15px" width="15px"> Pendentes</a></li>
+                  <li <?php if($active == "menu2") echo ' class="active"'; ?>><a data-toggle="pill" href="#menu2" style="color: #797979"><img src="images/agenda3.png" height="15px" width="15px"> Atrasados</a></li>
+                  <li <?php if($active == "menu3") echo ' class="active"'; ?>><a data-toggle="pill" href="#menu3" style="color: #797979;"><img src="images/agenda2.png" height="15px" width="15px"> Concluídos</a></li>
+                  <li <?php if($active == "menu4") echo ' class="active"'; ?>><a data-toggle="pill" href="#menu4" style="color: #797979;"><img src="images/agenda4.png" height="15px" width="15px"> Cancelados</a></li>
                 </ul>
                   
                 <div class="tab-content">
-                  <div id="home" class="tab-pane fade in active">
+                  <div id="home" class="tab-pane fade <?php if($active == "home") echo 'in active'; ?>">
                     <?php                            
                       $json_dados = $service->call('agendamento.select_sem_resposta_by_empresa',array($_SESSION["id"]));
                       $agendamento = json_decode($json_dados);
@@ -61,9 +79,9 @@
                       {
                     ?>
                     <section id="no-more-tables" style="margin-right:10px;margin-left:10px;">
-                       <form action="imprimir_agendamentos.php" method="post">
+                      <form action="imprimir_agendamentos.php" method="post" id="form_imprimir0">
                           <input type="hidden" value="em_espera" id="em_espera" name="em_espera">
-                          <a class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
+                         <a href="#" onclick="document.getElementById('form_imprimir0').submit();" class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
                        </form>
                       <table class="table table-striped table-condensed cf ">
                         <thead class="cf">
@@ -97,8 +115,8 @@
                                   <td data-title='Solicitante'>" . $usuario[0]->nome . "</td>
                                   <td data-title='Telefone'>" . $usuario[0]->telefone . "</td>
                                   <td data-title='E-mail'>" . $usuario[0]->email . "</td>                    
-                                  <td data-title='Aceitar'><form method='POST' action='tratar_agendamento.php'><input type='hidden' id='id' name='id' value=" . $agendamento[$i]->id . "><input type='hidden' id='aceitar' name='aceitar'><center><a type='submit'><img src='images/icones/icone-02.png' style='height:25px; width:25px;'></a></center></form></td>
-                                  <td data-title='Recusar'><form method='POST' action='tratar_agendamento.php'><input type='hidden' id='id' name='id' value=" . $agendamento[$i]->id . "><center><a type='submit' id='recusar' name='recusar'><img src='images/icones/icone-01.png' style='height:25px; width:25px;'></a></center></form></td>
+                                   <td data-title='Accept'><form method='POST' id='aceitar_agendamento". $agendamento[$i]->id ."' action='#'><input type='hidden' id='id' name='aceitar' value=" . $agendamento[$i]->id . "><center><a href='#' onclick=\"aceitara('aceitar_agendamento". $agendamento[$i]->id ."');\"><img src='images/icones/icone-02.png' style='height:25px; width:25px;'></a></center></form></td>
+                                  <td data-title='Refuse'><form method='POST' id='recusar_agendamento". $agendamento[$i]->id ."' action='#'><input type='hidden' id='id' name='recusar' value=" . $agendamento[$i]->id . "><center><a href='#' onclick=\"aceitara('recusar_agendamento". $agendamento[$i]->id ."');\" id='recusar' name='recusar'><img src='images/icones/icone-01.png' style='height:25px; width:25px;'></a></center></form></td>
                                 </tr>";
                         }
                     ?>
@@ -112,7 +130,7 @@
                         echo "<center><h4>Nenhum agendamento em espera.</h4></center><br>";
                     ?>
                   </div>
-                  <div id="menu1" class="tab-pane fade">
+                 <div id="menu1" class="tab-pane fade <?php if($active == "menu1") echo 'in active'; ?>">
                     <?php                            
 
                     $json_dados = $service->call('agendamento.select_aceitos_by_empresa',array($_SESSION["id"]));
@@ -122,9 +140,9 @@
                     {
                 ?>
                 <section id="no-more-tables" style="margin-right:10px;margin-left:10px;">
-                   <form action="imprimir_agendamentos.php" method="post">
+                   <form action="imprimir_agendamentos.php" method="post" id="form_imprimir1">
                         <input type="hidden" value="aceito" id="aceito" name="aceito">
-                        <a class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
+                        <a href="#" onclick="document.getElementById('form_imprimir1').submit();" class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
                    </form>
                   <table class="table table-striped table-condensed cf ">
                       <thead class="cf">
@@ -155,7 +173,7 @@
                               <td data-title="Solicitante">' . $usuario[0]->nome . '</td>
                               <td data-title="Telefone">' . $usuario[0]->telefone . '</td>
                               <td data-title="E-mail">' . $usuario[0]->email . '</td>
-                              <td data-title="Cancelar"><form method="POST" action="tratar_agendamento.php"><input type="hidden" id="id" name="id" value=' . $agendamento[$i]->id . '><input type="hidden" id="cancelar" name="cancelar"><center><a type="submit"><img src="images/icones/icone-01.png" style="height:25px; width:25px;"></a></center></form></td>
+                              <td data-title="Cancelar"><form method="POST" id="cancelar_agendamento'. $agendamento[$i]->id .'" action="#"><input type="hidden" id="id" name="cancelar" value="' . $agendamento[$i]->id . '"><center><a href="#" onclick="aceitara(`cancelar_agendamento'. $agendamento[$i]->id .'`);"><img src="images/icones/icone-01.png" style="height:25px; width:25px;"></a></center></form></td>
                             </tr>';
                     }
                   ?>
@@ -168,7 +186,7 @@
                       echo "<center><h4>Nenhum agendamento aceito.</h4></center><br>";
                   ?>
                   </div>
-                  <div id="menu2" class="tab-pane fade">
+                  <div id="menu2" class="tab-pane fade <?php if($active == "menu2") echo 'in active'; ?>">
                     <?php                            
                       $json_dados = $service->call('agendamento.select_atrasados_by_empresa',array($_SESSION["id"]));
                       $agendamento = json_decode($json_dados);
@@ -177,9 +195,9 @@
                       {
                   ?>
                   <section id="no-more-tables" style="margin-right:10px;margin-left:10px;">
-                      <form action="imprimir_agendamentos.php" method="post">
+                      <form action="imprimir_agendamentos.php" method="post" id="form_imprimir2">
                         <input type="hidden" value="atrasado" id="atrasado" name="atrasado">
-                           <a class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
+                            <a href="#" onclick="document.getElementById('form_imprimir2').submit();" class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
                       </form>
                     <table class="table table-striped table-condensed cf">
                         <thead class="cf">
@@ -210,7 +228,7 @@
                                   <td data-title="Solicitante">' . $usuario[0]->nome . '</td>
                                   <td data-title="Telefone">' . $usuario[0]->telefone . '</td>
                                   <td data-title="E-mail">' . $usuario[0]->email . '</td>
-                                  <td data-title="Recusar"><form method="POST" action="tratar_agendamento.php"><input type="hidden" id="id" name="id" value=' . $agendamento[$i]->id . '><input type="hidden" id="cancelar" name="cancelar"><center><a type="submit"><img src="images/icones/icone-01.png" style="height:25px; width:25px;"></a></center></form></td>
+                                  <td data-title="Cancelar"><form method="POST" id="cancelar_agendamento'. $agendamento[$i]->id .'" action="#"><input type="hidden" id="id" name="cancelar" value="' . $agendamento[$i]->id . '"><center><a href="#" onclick="aceitara(`cancelar_agendamento'. $agendamento[$i]->id .'`);");\"><img src="images/icones/icone-01.png" style="height:25px; width:25px;"></a></center></form></td>
                                 </tr>';
                         }
                     ?>
@@ -223,7 +241,7 @@
                         echo "<center><h4>Nenhhum agendamento atrasado. :)</h4></center><br>";
                     ?>
                   </div>
-                  <div id="menu3" class="tab-pane fade">
+                  <div id="menu3" class="tab-pane fade <?php if($active == "menu3") echo 'in active'; ?>">
                 <?php                            
                   $json_dados = $service->call('agendamento.select_realizados_by_empresa',array($_SESSION["id"]));
                   $agendamento = json_decode($json_dados);
@@ -233,9 +251,9 @@
                 ?>
                 
                 <section id="no-more-tables" style="margin-right:10px;margin-left:10px;">
-                   <form action="imprimir_agendamentos.php" method="post">
+                   <form action="imprimir_agendamentos.php" method="post" id="form_imprimir3">
                         <input type="hidden" value="realizado" id="realizado" name="realizado">
-                        <a class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
+                        <a href="#" onclick="document.getElementById('form_imprimir3').submit();" class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
                    </form>
                    <table class="table table-striped table-condensed cf ">
                       <thead class="cf">
@@ -277,7 +295,7 @@
                     echo "<center><h4>Nenhum agendamento realizado.</h4></center><br>";
                 ?>
                   </div>
-                  <div id="menu4" class="tab-pane fade">
+                 <div id="menu4" class="tab-pane fade <?php if($active == "menu4") echo 'in active'; ?>">
                     <?php                            
                   $json_dados = $service->call('agendamento.select_cancelados_by_empresa',array($_SESSION["id"]));
                   $agendamento = json_decode($json_dados);
@@ -287,9 +305,9 @@
                 ?>
                 
                 <section id="no-more-tables" style="margin-right:10px;margin-left:10px;">
-                   <form action="imprimir_agendamentos.php" method="post">
+                   <form action="imprimir_agendamentos.php" method="post" id="form_imprimir4">
                         <input type="hidden" value="cancelado" id="cancelado" name="cancelado">
-                        <a class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
+                        <a href="#" onclick="document.getElementById('form_imprimir4').submit();" class="pull-right" style="margin-right:30px;"><img src='images/icones/icone-08.png' style='margin-top:5px;height:25px; width:25px;'></a>
                    </form>
                    <table class="table table-striped table-condensed cf ">
                       <thead class="cf">
@@ -345,14 +363,14 @@
     <script src="assets/js/jquery-1.8.3.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="assets/js/jquery.sparkline.js"></script>
-
 
     <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
-  
+  <script>
+    function aceitara(ide)
+    {
+      document.getElementById(ide).submit();    
+    }
+  </script>  
 
   </body>
 </html>
