0d83afb16435518f3cf13df582f2163c8d317db8
occhapeco <occhapecosenai@gmail.com>
2016-10-30 19:28:33 -0200

    Perfumaria

--- eng/master/topnav.php
+++ eng/master/topnav.php
@@ -1,12 +1,60 @@
-      <header class="header black-bg">
-              
-            <!--logo start-->
-            <a href="index.html" class="logo"><b>DESCARTES</b></a>
-            <!--logo end-->
-           
-            <div class="top-menu">
-            	<ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="login.html">Logout</a></li>
-            	</ul>
-            </div>
-        </header>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Infinity - Mobile App HTML5 Template">
+	<meta name="keywords" content="keywords">
+	<meta name="author" content="Audain Designs">
+	<title>DescartesLab</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- General Style -->
+	<link href="style.css" rel="stylesheet">
+	
+	<!--Custom CSS-->
+	<link href="custom.css" rel="stylesheet">
+
+	<!-- Animations -->
+	<link href="css/animate.css" rel="stylesheet">
+	
+	<!-- Owl Carousel -->
+	<link href="css/owl.carousel.css" rel="stylesheet">
+	<link href="css/owl.theme.css" rel="stylesheet">
+	<link href="css/owl.transitions.css" rel="stylesheet">
+	
+	<!-- Vendor Styles -->
+	<link rel="stylesheet" type="text/css" href="vendor/push-menu/css/jasny-bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="vendor/push-menu/css/push-menu.css" />
+	<link rel="stylesheet" type="text/css" href="vendor/animated-text/css/style.css" />
+	
+	<!-- Device Styles -->
+	<link rel="stylesheet" type="text/css" href="css/devices/style.css">
+	<script src="https://use.fontawesome.com/9c8fd2c64e.js"></script>
+
+	<!-- Cover Styles (Skin) -->
+	<link rel="stylesheet" type="text/css" href="css/landing/landing.css">
+	
+	<!--Fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,200,100,500,600,700,800,900' rel='stylesheet' type='text/css'>
+	<link href="css/fonts/font-awesome.css" rel="stylesheet">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	
+	<style>
+	.mobile{
+		background-color:#606060;
+	}
+	</style>
+	
+<header class="mobile">
+  <div class="row">
+    <div class="col full">
+      <div class="logo"><img src="images/logo2.png" height="43px" width="130px" style="margin-top:5px; margin-left:30px;"></div>
+    </div>
+  </div>
+</header>
